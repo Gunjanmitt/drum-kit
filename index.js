62eb4e7faff6358cb6ed1931b902c4896b3a2b35
Gunjanmitt <gunimittal2005@gmail.com>
2025-05-27 04:07:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var buttonInnerHtml = this.innerHTML;
-    makeSound(buttonInnerHtml);
-    buttonAnimation(buttonInnerHtml);
-  });
-}
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
 
-document.addEventListener("keypress", function (event) {
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  var buttonInnerHtml = this.innerHTML ;
+        makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+  }
+ );}
+
+ 
+ 
+ document.addEventListener("keypress",function(event){
   makeSound(event.key);
   buttonAnimation(event.key);
 });
 
-function makeSound(key) {
-  switch (key) {
+
+
+function makeSound(key){
+  switch(key){
     case "w":
-      new Audio("tom-1.mp3").play();
-      break;
+        var tom1= new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
     case "a":
-      new Audio("tom-2.mp3").play();
+      var tom2= new Audio('sounds/tom-2.mp3');
+      tom2.play();
       break;
     case "s":
-      new Audio("tom-3.mp3").play();
+      var tom3= new Audio('sounds/tom-3.mp3');
+      tom3.play();
       break;
     case "d":
-      new Audio("tom-4.mp3").play();
+      var tom4= new Audio('sounds/tom-4.mp3');
+      tom4.play();
       break;
     case "j":
-      new Audio("crash.mp3").play();
+      var crash= new Audio('sounds/crash.mp3');
+      crash.play();
       break;
     case "k":
-      new Audio("kick-bass.mp3").play();
+      var kick= new Audio('sounds/kick-bass.mp3');
+      kick.play();
       break;
     case "l":
-      new Audio("snare.mp3").play();
-      break;
-    default:
-      console.log(key);
-  }
-}
+     var snare= new Audio('sounds/snare.mp3');
+     snare.play();
+     break;
+     default:console.log(key);
 
-function buttonAnimation(currentKey) {
-  var activeButton = document.querySelector("." + currentKey);
-  if (activeButton) {
-    activeButton.classList.add("pressed");
-    setTimeout(function () {
-      activeButton.classList.remove("pressed");
-    }, 100);
-  }
+}
+}
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  },100) ;
 }
